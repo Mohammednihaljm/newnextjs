2c6c443466b5803177075b7ba0788a4695bf4646
Mohammednihaljm <mohammednihaljm@gmail.com>
2023-08-23 11:24:23 +0400

    footercss

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -5,7 +5,7 @@ import Image from 'next/image'
 const Footer = () => {
   return (
     <div className={styles.container}>
-      <div>@Developed By Nihal</div>
+      <div className={styles.fname}>@Developed By Nihal JM</div>
       <div className={styles.social}>
         <Image src='/1.png' width={15} height={15} alt='fb'/>
         <Image src='/2.jpg' width={15} height={15} alt='insta'/>
